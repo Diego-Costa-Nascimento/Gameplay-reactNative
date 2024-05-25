1dfbe6e2982725cb4026211c15814f29be714b65
Diego-Costa-Nascimento <159532575+Diego-Costa-Nascimento@users.noreply.github.com>
2024-05-24 22:00:17 -0300

    dfv
    fdv

--- gameplay/src/screens/index.tsx
+++ gameplay/src/screens/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+
 export function SignIn(){
   return (
     <View>
@@ -8,3 +9,5 @@ export function SignIn(){
     </View>
   );
 }
+
+<View style = {{flex: 1, backgroundColor: 'red',alignItems: 'center', justifyContent: 'center' }}></View>
